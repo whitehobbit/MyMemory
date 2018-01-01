54909374ea513cc953a7020919d1f99478d7ce78
whitehobbit <limhabin@gmail.com>
2018-01-01 15:07:35 +0900

    Initial Commit

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by White Hobbit on 2018. 1. 1..
+//  Copyright © 2018년 WhiteHobbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
